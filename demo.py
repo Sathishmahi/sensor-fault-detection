ef1685b776289f936fdddc368329c9c295ab8528
Sathishmahi <sathishmahi433@gmail.com>
2022-11-26 18:22:16 +0530

    all needed folders and files create

---
+++ demo.py
@@ -0,0 +1,7 @@
+
+with open('/config/workspace/sensor-fault-detection/requirements.txt') as f:
+    a=f.readlines()
+    li=[i.strip() for i in a ]
+print(li)
+    
+  
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+    packages=find_packages(),
+    author_name="sathish",
+    author_email="sathishmahi433@gamil.com",
+    version="0.0.1",
+    name="sensor",
+    install_requires=[]
+)
+
